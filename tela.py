4ad272830e1fe7856671ad29652ece2128d0d52c
matheuspn <mat.procopip69@gmail.com>
2021-01-04 19:01:45 -0300

    initial

---
+++ tela.py
@@ -0,0 +1,44 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+
+import asyncio
+from downloader import baixar, get_info
+
+
+class Tela1(GridLayout):
+    
+    def download(self):
+        self.ids.status.text = "baixando ..."
+        url = self.ids.url_text.text
+        asyncio.create_task(Tela1.info(self,url))
+        asyncio.create_task(Tela1.baixa(self, url))
+
+
+    # chama a função download 
+    async def baixa(self, url):
+        #acabou = await asyncio.to_thread(baixar, url= url)
+        self.ids.status.text = "concluído"
+
+    # pega as informações do vídeo baixado
+    async def info(self, url):
+        meta = await asyncio.to_thread(get_info, url= url)
+        # adiciona a thumb na tela com o título
+        self.ids.box.add_widget(Video(video_text= meta[0], video_image= meta[1]))
+
+class Video(BoxLayout):
+    def __init__(self,video_text='',video_image= '',**kwargs):
+        super().__init__(**kwargs)
+        self.ids.video_text.text = video_text
+        self.ids.video_image.source = video_image
+
+class Tela(App):
+    def build(self):
+        return Tela1()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(Tela().async_run(async_lib='asyncio'))
+    loop.close()
+
+# https://www.youtube.com/watch?v=sJSR5-VOlRc
