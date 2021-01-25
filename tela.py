95030bd338b58af2b5c3b4859ccee14bf72dd2c8
matheuspn <mat.procopip69@gmail.com>
2021-01-25 18:59:10 -0300

    using KivyMD

--- tela.py
+++ tela.py
@@ -1,18 +1,17 @@
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
 
 import asyncio
 from downloader import baixar, get_info
 
 
-class Tela1(BoxLayout):
+class Home(MDBoxLayout):
     
     def download(self):
         self.ids.status.text = "baixando ..."
         url = self.ids.url_text.text
-        asyncio.create_task(Tela1.info(self,url))
-        asyncio.create_task(Tela1.baixa(self, url))
+        asyncio.create_task(Home.info(self,url))
+        asyncio.create_task(Home.baixa(self, url))
 
 
     # chama a função download 
@@ -26,15 +25,16 @@ class Tela1(BoxLayout):
         # adiciona a thumb na tela com o título
         self.ids.box.add_widget(Video(video_text= meta[0], video_image= meta[1]))
 
-class Video(BoxLayout):
+class Video(MDBoxLayout):
     def __init__(self,video_text='',video_image= '',**kwargs):
         super().__init__(**kwargs)
         self.ids.video_text.text = video_text
         self.ids.video_image.source = video_image
 
-class Tela(App):
+class Tela(MDApp):
     def build(self):
-        return Tela1()
+        self.theme_cls.theme_style = "Dark"
+        return Home()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
